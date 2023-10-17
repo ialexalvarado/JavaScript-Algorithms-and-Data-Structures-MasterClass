b7b00ee419f0c6f5d5404d6860d135a841702cf8
ialexalvarado <ialejandroalvarado@gmail.com>
2023-10-16 18:10:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaScript-Algorithms-and-Data-Structures-MasterClass
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/ialexalvarado/JavaScript-Algorithms-and-Data-Structures-MasterClass)
---
+++ index.js
@@ -0,0 +1,21 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+
+function sumZero(arr){
+  let left=0
+  let right=arr.length - 1
+  while(left < right){
+    let sum = arr[left] + arr[right]
+    if(sum == 0){
+      return [arr[left], arr[right]]
+    } else if (sum > 0){
+      right--
+    }else{
+      left++
+    }
+  }
+}
+
+console.log(sumZero([-4,-3,-2,-1,0,1,2,3,10]))
